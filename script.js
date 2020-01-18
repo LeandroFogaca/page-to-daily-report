59731bcb0eb6bf0f115c2e87af59144d22d54d40
Leandro Fogaça <fogaca.lf@gmail.com>
2020-01-18 11:17:48 -0300

    ajuste de layout realizado
    foi utilizado a class grid para dividir em colunas

---
+++ script.js
@@ -0,0 +1,14 @@
+
+function criaratividade (maquina, descrição, tempo) {
+    
+        return {
+            maquina,
+            descrição,
+            tempo
+        }
+
+}
+
+
+console.log(criaratividade("EBM01", "destravado moinho", 2));
+console.log(criaratividade("EBM02", "destravado moinho", 2));
