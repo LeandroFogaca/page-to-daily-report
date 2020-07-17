3e30de2e4370a17797b54012f8d438a2fd612d89
Leandro Fogaça <fogaca.lf@gmail.com>
2020-07-16 22:02:47 -0300

    tentativa de usar objeto

--- script.js
+++ script.js
@@ -1,29 +1,51 @@
 const report = []
 var contador = 0
 
+function Atividades(os, maq, desc, acao){
+
+    this.osnumber = os;
+    this.machine = maq;
+    this.descricao = desc;
+    this.acao = acao;
+}
 
 
-
-
-
-
-function adicionar() {
-
-    let atividade = []
-
-    contador += 1
+function coleta(){
     
+    let atividade = []
     atividade.push(document.getElementById("osnumber").value)
     atividade.push(document.getElementById("machine").value)
     atividade.push(document.getElementById("description").value)
     atividade.push(document.getElementById("action").value)
+    
+    atividade[0] = `Numero OS: ${atividade[0]}`
+    atividade[1] = `Máquina:   ${atividade[1]}`
+    atividade[2] = `Descrição: ${atividade[2]}`
+    atividade[3] = `Ação:      ${atividade[3]}`
+    
+   // console.log(atividade);
     report.push(atividade)
+}
+
+
+function adicionar() {
     
     
-    document.getElementById("conteudo").innerHTML = report
+    let os = document.getElementById("osnumber").value
+    let maq = document.getElementById("machine").value
+    let desc = document.getElementById("description").value
+    let act = document.getElementById("action").value
+    
+    a = new Atividades(os,maq,desc,act)
     
     
-    console.log(atividade);
-    console.log(report);
+    
+    //coleta()
+    
+    let msg = 'Numero da OS:' + a.os
+    console.log(msg);
+    document.getElementById("conteudo").innerHTML = msg
+    
+   
 }
 
